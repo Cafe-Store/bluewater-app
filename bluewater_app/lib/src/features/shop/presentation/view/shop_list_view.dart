2e6dc7a9ce5be513195087d745c21d9a7deb9fa6
handroid <handroid@lotte.net>
2021-08-12 17:53:09 +0900

    err : 오타수정

--- bluewater_app/lib/src/features/shop/presentation/view/shop_list_view.dart
+++ bluewater_app/lib/src/features/shop/presentation/view/shop_list_view.dart
@@ -1,7 +1,8 @@
+import 'package:bluewater_app/src/features/shop/presentation/controller/shop_controller.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
-import '../controller/shop_contoller.dart';
+import '../controller/shop_controller.dart';
 
 class ShopListView extends GetView<ShopController> {
   const ShopListView({Key? key}) : super(key: key);

7b0b3882cf765178d8e4d526a4666fa5f8efec5e
bambam <green5940@gmail.com>
2021-10-07 08:47:33 +0900

    feat: shimmer 적용중

--- bluewater_app/lib/src/features/categories/presentation/widget/categories_widget.dart
+++ bluewater_app/lib/src/features/categories/presentation/widget/categories_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 
 import '../../../../core/util/const.dart';
 import '../../../../routes/app_pages.dart';
+import '../../../../shared/ui/widget/shimmer/shimmer_loading.dart';
 import '../../domain/entity/category.dart';
 import '../service/categories_service.dart';
 import 'category_item.dart';
@@ -16,40 +16,48 @@ class CategoriesWidget extends GetView<CategoriesService> {
   Widget build(BuildContext context) {
     return Obx(
       () {
-        if (controller.datas.isEmpty) {
-          return Center(
-            child: SpinKitThreeBounce(
-              color: Colors.grey,
-              size: 15,
+        return ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ShimmerLoading(
+              isLoading: controller.datas.isEmpty,
+              child: controller.datas.isNotEmpty
+                  ? _createCategoryItem(index, context)
+                  : SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
             ),
-          );
-        } else {
-          return ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: InkWell(
-                onTap: () => Get.rootDelegate.toNamed(
-                  Routes.shops,
-                  parameters: {
-                    RoutesParamName.selectedCategoryName:
-                        controller.datas[index].name,
-                    RoutesParamName.showCatagoryTab: true.toString(),
-                  },
-                ),
-                child: CategoryItem<Category>(
-                  item: controller.datas[index],
-                  titleStyle: Theme.of(context)
-                      .textTheme
-                      .bodyText1!
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            itemCount: controller.datas.length,
-          );
-        }
+          ),
+          itemCount: controller.datas.isNotEmpty ? controller.datas.length : 10,
+        );
       },
+    );
+  }
+
+  InkWell _createCategoryItem(int index, BuildContext context) {
+    return InkWell(
+      onTap: () => Get.rootDelegate.toNamed(
+        Routes.shops,
+        parameters: {
+          RoutesParamName.selectedCategoryName: controller.datas[index].name,
+          RoutesParamName.showCatagoryTab: true.toString(),
+        },
+      ),
+      child: CategoryItem<Category>(
+        item: controller.datas[index],
+        titleStyle: Theme.of(context)
+            .textTheme
+            .bodyText1!
+            .copyWith(fontWeight: FontWeight.bold),
+      ),
     );
   }
 }

0892dc4f62e24ab0f71e950408f5ee68878738a6
bambam <green5940@gmail.com>
2021-10-08 16:08:54 +0900

    feat: shimmer 적용

--- bluewater_app/lib/src/features/shops/presentation/categorized_shops/widget/categorized_shops_widget.dart
+++ bluewater_app/lib/src/features/shops/presentation/categorized_shops/widget/categorized_shops_widget.dart
@@ -1,11 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 
 import '../../../../../core/util/const.dart';
 import '../../../../../routes/app_pages.dart';
+import '../../../../../shared/ui/widget/shimmer/shimmer_loading.dart';
 import '../../../domain/entity/shop.dart';
 import '../controller/categorized_shops_controller.dart';
 
@@ -25,65 +25,107 @@ class CategorizedShopsWidget extends GetWidget<CategorizedShopsController> {
   Widget build(BuildContext context) {
     return Obx(
       () {
-        if (controller.datas.isEmpty) {
-          return Center(
-            child: SpinKitThreeBounce(
-              color: Colors.grey,
-              size: 15,
-            ),
-          );
-        } else {
-          return Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        title,
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
+        return Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (controller.datas.isNotEmpty) {
+                        Get.rootDelegate.toNamed(
+                          Routes.shops,
+                          parameters: {
+                            RoutesParamName.selectedCategoryName: title,
+                          },
+                        );
+                      }
+                    },
+                    icon: Icon(
+                      Icons.arrow_forward_rounded,
+                      color: Colors.black54,
                     ),
-                    IconButton(
-                      onPressed: () => Get.rootDelegate.toNamed(
-                        Routes.shops,
-                        parameters: {
-                          RoutesParamName.selectedCategoryName: title,
-                        },
-                      ),
-                      icon: Icon(
-                        Icons.arrow_forward_rounded,
-                        color: Colors.black54,
-                      ),
-                    )
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        if (index == 9) {
-                          return _createMoreButton();
-                        } else {
-                          return Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 10.0),
-                            child: _ShopItem(shop: controller.datas[index]),
-                          );
-                        }
-                      },
-                      itemCount: 10),
-                ),
-              ],
-            ),
-          );
-        }
+                  )
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    physics: controller.datas.isEmpty
+                        ? NeverScrollableScrollPhysics()
+                        : null,
+                    itemBuilder: (context, index) {
+                      if (index == 9) {
+                        return _createMoreButton();
+                      } else {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: ShimmerLoading(
+                            isLoading: controller.datas.isEmpty,
+                            child: controller.datas.isNotEmpty
+                                ? _ShopItem(shop: controller.datas[index])
+                                : _createDefaultShopItem(context),
+                          ),
+                        );
+                      }
+                    },
+                    itemCount: 10),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
 
-  Center _createMoreButton() {
+  Widget _createDefaultShopItem(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          flex: 10,
+          child: Container(
+            constraints: BoxConstraints(minHeight: 150, minWidth: 150),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Spacer(),
+        Text(
+          '가게 이름',
+          style: Theme.of(context)
+              .textTheme
+              .bodyText1!
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
+        Row(
+          children: [
+            const Icon(
+              Icons.star,
+              color: Colors.amber,
+              size: 15,
+            ),
+            Text(
+              '0.0(0)',
+              style:
+                  Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 12),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _createMoreButton() {
     return Center(
       child: SizedBox.fromSize(
         size: Size(60, 60),

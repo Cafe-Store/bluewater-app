8958374be2fbb8d30861920d20e0164634e4d65e
bambam <green5940@gmail.com>
2021-10-14 21:16:00 +0900

    refact: 릴리즈 모드로 시작할 때, 가로 세로 크기 0 이슈 대응하기 위해 수정.

--- bluewater_app/lib/src/main.dart
+++ bluewater_app/lib/src/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 
 import 'core/injector/injection.dart';
 import 'core/util/const.dart';
@@ -21,6 +23,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp.router(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, widget!),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+      ),
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.dartkTheme,
       getPages: AppPages.routes,

2e9893ffcb37919ec1ce1b241f7796ef17fde397
bambam <green5940@gmail.com>
2021-10-25 21:51:47 +0900

    refact: json key ownerId로 받을 수 있도록 수정

--- bluewater_app/lib/src/features/shops/domain/entity/owner.dart
+++ bluewater_app/lib/src/features/shops/domain/entity/owner.dart
@@ -1,7 +1,10 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 class Owner extends Equatable {
+  @JsonKey(name: 'ownerId')
   final String id;
+
   final String name;
 
   Owner(this.id, this.name);
